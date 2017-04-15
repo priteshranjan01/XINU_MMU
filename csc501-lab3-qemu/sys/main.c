28522257c0b0b314a5cf3db109b12cd634e886d5
Pritesh Ranjan <pranjan@ncsu.edu>
2017-04-15 16:14:58 -0400

    Testcase 3 works only after we have test case 1

--- csc501-lab3-qemu/sys/main.c
+++ csc501-lab3-qemu/sys/main.c
@@ -19,7 +19,7 @@ void proc1_test1(char *msg, int lck) {
 
 	if (xmmap(PROC1_VPNO, TEST1_BS, 100) == SYSERR) {
 		kprintf("xmmap call failed\n");
-		sleep(3);
+		sleep(1);
 		return;
 	}
 
@@ -28,14 +28,37 @@ void proc1_test1(char *msg, int lck) {
 		*(addr + i * NBPG) = 'A' + i;
 	}
 
-	sleep(6);
+	sleep(1);
 
 	for (i = 0; i < 26; i++) {
 		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
+   
+  kprintf("\nNow there should be no page faults");
+  for (i = 0; i < 26; i++) {
+		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + 25 * NBPG));}
+   
 	}
 
 
 	xmunmap(PROC1_VPNO);
+	return;
+}
+
+void proc1_test3(char *msg, int lck) {
+
+	char *addr;
+	int i;
+
+	addr = (char*) 0x0;
+
+	for (i = 0; i < 1024; i++) {
+		*(addr + i * NBPG) = 'B';
+	}
+
+	for (i = 0; i < 1024; i++) {
+		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
+	}
+
 	return;
 }
 
@@ -46,6 +69,13 @@ int main() {
 	kprintf("\n1: shared memory\n");
 	pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
 	resume(pid1);
-	sleep(10);
+	sleep(5);
+ //pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
+//	resume(pid1);
+//	sleep(10);
+ kprintf("\n3: Frame test\n");
+	pid1 = create(proc1_test3, 2000, 20, "proc1_test3", 0, NULL);
+	resume(pid1);
+	sleep(3);
  shutdown();
  }
