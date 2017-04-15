579aa8b3ae4d3a9e8a131c56a0e4eee9acb3403d
Pritesh Ranjan <pranjan@ncsu.edu>
2017-04-15 13:24:54 -0400

    xunmap fails in this because bsm_unmap returns SYSERR

--- csc501-lab3-qemu/sys/main.c
+++ csc501-lab3-qemu/sys/main.c
@@ -11,44 +11,41 @@
 #define PROC2_VPNO      0x80000
 #define TEST1_BS	1
 
-/*------------------------------------------------------------------------
- *  main  --  user main program
- *------------------------------------------------------------------------
- */
- 
- void proc1_test1(char *msg, int lck) {
- kprintf("\n A walk around the lake");
- char *addr;
+void proc1_test1(char *msg, int lck) {
+	char *addr;
 	int i;
+
 	get_bs(TEST1_BS, 100);
+
 	if (xmmap(PROC1_VPNO, TEST1_BS, 100) == SYSERR) {
 		kprintf("xmmap call failed\n");
 		sleep(3);
 		return;
 	}
+
 	addr = (char*) PROC1_VADDR;
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 26; i++) {
 		*(addr + i * NBPG) = 'A' + i;
 	}
 
 	sleep(6);
 
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 26; i++) {
 		kprintf("0x%08x: %c\n", addr + i * NBPG, *(addr + i * NBPG));
 	}
- }
- 
-int main()
-{
-	int pid1;
-	kprintf("\n\nHello World, Xinu@QEMU lives\n\n");
 
-        /* The hook to shutdown QEMU for process-like execution of XINU.
-         * This API call terminates the QEMU process.
-         */
-	kprintf("\n1: shared memory\n");
-	pid1 = create(proc1_test1, 2000, 25, "proc1_test1", 0, NULL);
-	resume(pid1);
-	sleep(14);
-        shutdown();
+
+	xmunmap(PROC1_VPNO);
+	return;
 }
+
+int main() {
+	int pid1;
+	int pid2;
+
+	kprintf("\n1: shared memory\n");
+	pid1 = create(proc1_test1, 2000, 20, "proc1_test1", 0, NULL);
+	resume(pid1);
+	sleep(10);
+ shutdown();
+ }
